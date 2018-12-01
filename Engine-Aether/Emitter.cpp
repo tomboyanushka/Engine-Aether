c739c74f69d8d3fd919ac211dc4c301ec2f1b1b9
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-11-30 21:40:39 -0500

    Emitter [progress]

--- Engine-Aether/Emitter.cpp
+++ Engine-Aether/Emitter.cpp
@@ -76,7 +76,7 @@ Emitter::Emitter(unsigned int maxParticles, float emissionRate, float lifetime, 
 		poolSRVDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
 		poolSRVDesc.Buffer.FirstElement = 0;
 		poolSRVDesc.Buffer.NumElements = maxParticles;
-		// DO NOT SET THESE because it is unioned with above data and will just overwrite correct values with incorrect values
+		// DO NOT SET THESE because it is union'd with above data and will just overwrite correct values with incorrect values
 		//poolSRVDesc.Buffer.ElementOffset = 0;
 		//poolSRVDesc.Buffer.ElementWidth = sizeof(Particle);
 		poolSRVDesc.Format = DXGI_FORMAT_UNKNOWN;
@@ -87,10 +87,114 @@ Emitter::Emitter(unsigned int maxParticles, float emissionRate, float lifetime, 
 	}
 
 	//Dead List 
-	{}
+	{
+		// Buffer
+		ID3D11Buffer* deadListBuffer;
+		D3D11_BUFFER_DESC deadDesc = {};
+		deadDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS;
+		deadDesc.ByteWidth = sizeof(unsigned int) * maxParticles;
+		deadDesc.CPUAccessFlags = 0;
+		deadDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+		deadDesc.StructureByteStride = sizeof(unsigned int);
+		deadDesc.Usage = D3D11_USAGE_DEFAULT;
+		device->CreateBuffer(&deadDesc, 0, &deadListBuffer);
+
+		// UAV
+		D3D11_UNORDERED_ACCESS_VIEW_DESC deadUAVDesc = {};
+		deadUAVDesc.Format = DXGI_FORMAT_UNKNOWN; 
+		deadUAVDesc.Buffer.FirstElement = 0;
+		deadUAVDesc.Buffer.Flags = D3D11_BUFFER_UAV_FLAG_APPEND; // Append/Consume
+		deadUAVDesc.Buffer.NumElements = maxParticles;
+		deadUAVDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+		device->CreateUnorderedAccessView(deadListBuffer, &deadUAVDesc, &particleDeadUAV);
+
+		//Release
+		deadListBuffer->Release();
+	}
 
 	//Draw List
-	{}
+	{
+		// Buffer
+		ID3D11Buffer* drawListBuffer;
+		D3D11_BUFFER_DESC drawDesc = {};
+		drawDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+		drawDesc.ByteWidth = sizeof(ParticleSort) * maxParticles;
+		drawDesc.CPUAccessFlags = 0;
+		drawDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+		drawDesc.StructureByteStride = sizeof(ParticleSort);
+		drawDesc.Usage = D3D11_USAGE_DEFAULT;
+		device->CreateBuffer(&drawDesc, 0, &drawListBuffer);
+
+		// UAV
+		D3D11_UNORDERED_ACCESS_VIEW_DESC drawUAVDesc = {};
+		drawUAVDesc.Format = DXGI_FORMAT_UNKNOWN; 
+		drawUAVDesc.Buffer.FirstElement = 0;
+		drawUAVDesc.Buffer.Flags = D3D11_BUFFER_UAV_FLAG_COUNTER; // IncrementCounter() in HLSL
+		drawUAVDesc.Buffer.NumElements = maxParticles;
+		drawUAVDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+		device->CreateUnorderedAccessView(drawListBuffer, &drawUAVDesc, &particleDrawUAV);
+
+		// SRV (for indexing in VS)
+		D3D11_SHADER_RESOURCE_VIEW_DESC drawSRVDesc = {};
+		drawSRVDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+		drawSRVDesc.Buffer.FirstElement = 0;
+		drawSRVDesc.Buffer.NumElements = maxParticles;
+		drawSRVDesc.Format = DXGI_FORMAT_UNKNOWN;
+		device->CreateShaderResourceView(drawListBuffer, &drawSRVDesc, &particleDrawSRV);
+
+		//Release
+		drawListBuffer->Release();
+	}
+
+	//Draw Args
+	{
+		// Buffer
+		D3D11_BUFFER_DESC argsDesc = {};
+		argsDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS;
+		argsDesc.ByteWidth = sizeof(unsigned int) * 5; // Need 5 if using an index buffer
+		argsDesc.CPUAccessFlags = 0;
+		argsDesc.MiscFlags = D3D11_RESOURCE_MISC_DRAWINDIRECT_ARGS;
+		argsDesc.StructureByteStride = 0;
+		argsDesc.Usage = D3D11_USAGE_DEFAULT;
+		device->CreateBuffer(&argsDesc, 0, &drawArgsBuffer);
+
+		// UAV
+		D3D11_UNORDERED_ACCESS_VIEW_DESC argsUAVDesc = {};
+		argsUAVDesc.Format = DXGI_FORMAT_R32_UINT; 
+		argsUAVDesc.Buffer.FirstElement = 0;
+		argsUAVDesc.Buffer.Flags = 0;  
+		argsUAVDesc.Buffer.NumElements = 5; // Need 5 if using an index buffer
+		argsUAVDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+		device->CreateUnorderedAccessView(drawArgsBuffer, &argsUAVDesc, &drawArgsUAV);
+
+	}
+	// Launch the dead list init shader
+	deadListInitCS->SetInt("MaxParticles", maxParticles);
+	deadListInitCS->SetUnorderedAccessView("DeadList", particleDeadUAV);
+	deadListInitCS->SetShader();
+	deadListInitCS->CopyAllBufferData();
+	deadListInitCS->DispatchByThreads(maxParticles, 1, 1);
+
+
+	// Additive blend state
+	D3D11_BLEND_DESC blend = {};
+	blend.AlphaToCoverageEnable = false;
+	blend.IndependentBlendEnable = false;
+	blend.RenderTarget[0].BlendEnable = true;
+	blend.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+	blend.RenderTarget[0].SrcBlend = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+	blend.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+	device->CreateBlendState(&blend, &additiveBlend);
+
+	D3D11_DEPTH_STENCIL_DESC depth = {};
+	depth.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
+	depth.DepthFunc = D3D11_COMPARISON_LESS;
+	depth.DepthEnable = true;
+	device->CreateDepthStencilState(&depth, &depthWriteOff);
 }
 
 Emitter::~Emitter()
